91d7aef6f3329292e1177dc7251ee07d5a29c6e6
Kate Mikhaylova <kate.mikhaylova@mail.ru>
2023-04-01 14:34:39 +0300

    signals added from models to separate file

--- procurement_supply/models.py
+++ procurement_supply/models.py
@@ -5,9 +5,6 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.db.models.signals import post_save, pre_save
-from django.dispatch import receiver
-from procurement_supply.tasks import send_email
 
 
 USER_TYPE_CHOICES = (
@@ -455,50 +452,3 @@ class PasswordResetToken(models.Model):
 
     def __str__(self):
         return f"Password reset token for user {self.user}"
-
-
-@receiver(pre_save, sender=OrderPosition)
-def cache_previous_order_position_status(sender, instance, **kwargs):
-    if instance.id:
-        order_position = sender.objects.get(pk=instance.id)
-        instance.__original_confirmed = order_position.confirmed
-        instance.__original_delivered = order_position.delivered
-
-
-@receiver(post_save, sender=OrderPosition)
-def send_email_change_order_pos_status(sender, instance, created, **kwargs):
-    if not created:
-        if (instance.__original_confirmed == instance.confirmed) \
-                and (instance.__original_delivered == instance.delivered):
-            return
-        if (instance.__original_confirmed and not instance.confirmed) \
-                or (instance.__original_delivered and not instance.delivered):
-            send_email.delay(
-                "Order position confirmation and/or delivery status revoked",
-                f'''Please contact supplier of position {instance.id} from your order #{instance.order.id}.
-                 Confirmation and/or delivery status or both of this position was revoked through admin site''',
-                instance.order.purchaser.user.email,
-            )
-            return
-        text = f'Your order #{instance.order.id} position {instance.id} was\n'
-        if not instance.__original_confirmed and instance.confirmed:
-            text += '- confirmed\n'
-        if not instance.__original_delivered and instance.delivered:
-            text += '- delivered\n'
-        text += """by supplier.
-        Order is confirmed when all positions are confirmed and delivered after all position are delivered"""
-        send_email.delay(
-            "Order position confirmation and/or delivery status changed",
-            text,
-            instance.order.purchaser.user.email
-        )
-
-
-@receiver(post_save, sender=User)
-def send_email_new_user(sender, instance, created, **kwargs):
-    if created:
-        send_email.delay(
-            "Welcome to our site",
-            f"""Thank you for your registration, {instance.username}.""",
-            instance.email
-        )
